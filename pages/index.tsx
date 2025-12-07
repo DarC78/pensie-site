832552aee2a34d02e35496e64893e949f04aa438
DarC78 <adrian@proveitweb.co.uk>
2025-12-07 20:51:00 +0000

    Initial Next.js setup

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontSize: 24 }}>
+      <h1>Bine ai venit la Pensie Comunitară</h1>
+      <p>Site-ul este în construcție. Pagini dinamice se vor genera automat.</p>
+    </div>
+  );
+}
